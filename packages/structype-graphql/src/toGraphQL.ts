402502f26b98d4aaa3df822143635d1d74ed8323
Chris Feijoo <chris@kube.io>
2024-12-16 14:25:11 +0100

    More cleanup on toGraphql.ts

--- packages/structype-graphql/src/toGraphQL.ts
+++ packages/structype-graphql/src/toGraphQL.ts
@@ -9,6 +9,7 @@ import {
   Schema_Union,
 } from "@kube/structype";
 import * as graphql from "graphql";
+import { Kind } from "graphql";
 
 function toGraphql_NamedType(schema: Schema): graphql.NamedTypeNode {
   let typeName;
@@ -38,74 +39,67 @@ function toGraphql_NamedType(schema: Schema): graphql.NamedTypeNode {
       throw new Error(`Unsupported type: ${schema._structype}`);
   }
   return {
-    kind: graphql.Kind.NAMED_TYPE,
-    name: { kind: graphql.Kind.NAME, value: typeName },
+    kind: Kind.NAMED_TYPE,
+    name: toGraphql_Name(typeName),
   };
 }
 
-function toGraphql_Description(description: string | undefined) {
+function toGraphql_Name(name: string): graphql.NameNode {
+  return {
+    kind: Kind.NAME,
+    value: name,
+  };
+}
+
+function toGraphql_Description(
+  description: string | undefined
+): graphql.StringValueNode | undefined {
   return description
-    ? { kind: graphql.Kind.STRING as const, value: description }
+    ? {
+        kind: Kind.STRING,
+        value: description,
+      }
     : undefined;
 }
 
-function nullableIf<T extends graphql.TypeNode>(
+function toGraphql_Nullable_If<T extends graphql.TypeNode>(
   condition: boolean | undefined,
   type: T
 ) {
-  return condition ? type : { kind: graphql.Kind.NON_NULL_TYPE as const, type };
-}
-
-function toGraphql_from_Schema_Record_Field(
-  schemaField: Schema_Record["fields"][number]
-): graphql.FieldDefinitionNode {
-  // Array
-
-  if (schemaField.type._structype === "array") {
-    return {
-      kind: graphql.Kind.FIELD_DEFINITION,
-      name: { kind: graphql.Kind.NAME, value: schemaField.name },
-      type: nullableIf(
-        schemaField.nullable,
-        toGraphql_from_Schema_Array(schemaField.type)
-      ),
-      arguments: schemaField.args?.map((arg) => {
-        return {
-          kind: graphql.Kind.INPUT_VALUE_DEFINITION,
-          name: { kind: graphql.Kind.NAME, value: arg.name },
-          type: nullableIf(arg.nullable, toGraphql_NamedType(arg.type)),
-          description: toGraphql_Description(arg.description),
-        };
-      }),
-      description: toGraphql_Description(schemaField.description),
-    };
-  }
-
-  // Named Type
-
-  return {
-    kind: graphql.Kind.FIELD_DEFINITION,
-    name: { kind: graphql.Kind.NAME, value: schemaField.name },
-    type: nullableIf(
-      schemaField.nullable,
-      toGraphql_NamedType(schemaField.type)
-    ),
-    arguments: schemaField.args?.map((arg) => ({
-      kind: graphql.Kind.INPUT_VALUE_DEFINITION,
-      name: { kind: graphql.Kind.NAME, value: arg.name },
-      type: nullableIf(arg.nullable, toGraphql_NamedType(arg.type)),
-      description: toGraphql_Description(arg.description),
-    })),
-    description: toGraphql_Description(schemaField.description),
-  };
+  return condition ? type : { kind: Kind.NON_NULL_TYPE as const, type };
 }
 
 function toGraphql_from_Schema_Array(
   schema: Schema_Array
 ): graphql.ListTypeNode {
   return {
-    kind: graphql.Kind.LIST_TYPE,
-    type: nullableIf(schema.nullableItems, toGraphql_NamedType(schema.item)),
+    kind: Kind.LIST_TYPE,
+    type: toGraphql_Nullable_If(
+      schema.nullableItems,
+      toGraphql_NamedType(schema.item)
+    ),
+  };
+}
+
+function toGraphql_from_Schema_Record_Field(
+  schemaField: Schema_Record["fields"][number]
+): graphql.FieldDefinitionNode {
+  return {
+    kind: Kind.FIELD_DEFINITION,
+    name: toGraphql_Name(schemaField.name),
+    type: toGraphql_Nullable_If(
+      schemaField.nullable,
+      schemaField.type._structype === "array"
+        ? toGraphql_from_Schema_Array(schemaField.type)
+        : toGraphql_NamedType(schemaField.type)
+    ),
+    arguments: schemaField.args?.map((arg) => ({
+      kind: Kind.INPUT_VALUE_DEFINITION,
+      name: toGraphql_Name(arg.name),
+      type: toGraphql_Nullable_If(arg.nullable, toGraphql_NamedType(arg.type)),
+      description: toGraphql_Description(arg.description),
+    })),
+    description: toGraphql_Description(schemaField.description),
   };
 }
 
@@ -113,8 +107,8 @@ function toGraphql_from_Schema_Record(
   schema: Schema_Record
 ): graphql.ObjectTypeDefinitionNode {
   return {
-    kind: graphql.Kind.OBJECT_TYPE_DEFINITION,
-    name: { kind: graphql.Kind.NAME, value: schema.name },
+    kind: Kind.OBJECT_TYPE_DEFINITION,
+    name: toGraphql_Name(schema.name),
     fields: schema.fields.map(toGraphql_from_Schema_Record_Field),
     description: toGraphql_Description(schema.description),
     interfaces: schema.implements?.map(toGraphql_NamedType),
@@ -125,8 +119,8 @@ function toGraphql_from_Schema_Interface(
   schema: Schema_Interface
 ): graphql.InterfaceTypeDefinitionNode {
   return {
-    kind: graphql.Kind.INTERFACE_TYPE_DEFINITION,
-    name: { kind: graphql.Kind.NAME, value: schema.name },
+    kind: Kind.INTERFACE_TYPE_DEFINITION,
+    name: toGraphql_Name(schema.name),
     fields: schema.fields.map(toGraphql_from_Schema_Record_Field),
     description: toGraphql_Description(schema.description),
   };
@@ -136,11 +130,11 @@ function toGraphql_from_Schema_Enum(
   schema: Schema_Enum
 ): graphql.EnumTypeDefinitionNode {
   return {
-    kind: graphql.Kind.ENUM_TYPE_DEFINITION,
-    name: { kind: graphql.Kind.NAME, value: schema.name },
+    kind: Kind.ENUM_TYPE_DEFINITION,
+    name: toGraphql_Name(schema.name),
     values: schema.values.map((value) => ({
-      kind: graphql.Kind.ENUM_VALUE_DEFINITION,
-      name: { kind: graphql.Kind.NAME, value },
+      kind: Kind.ENUM_VALUE_DEFINITION,
+      name: toGraphql_Name(value),
     })),
     description: toGraphql_Description(schema.description),
   };
@@ -156,8 +150,8 @@ function toGraphql_from_Schema_Union(
     throw new Error("Union must have a name for GraphQL");
   }
   return {
-    kind: graphql.Kind.UNION_TYPE_DEFINITION,
-    name: { kind: graphql.Kind.NAME, value: schema.name },
+    kind: Kind.UNION_TYPE_DEFINITION,
+    name: toGraphql_Name(schema.name),
     types: schema.types.map(toGraphql_NamedType),
     description: toGraphql_Description(schema.description),
   };
@@ -167,12 +161,15 @@ function toGraphql_from_Schema_Input(
   schema: Schema_Input
 ): graphql.InputObjectTypeDefinitionNode {
   return {
-    kind: graphql.Kind.INPUT_OBJECT_TYPE_DEFINITION,
-    name: { kind: graphql.Kind.NAME, value: schema.name },
+    kind: Kind.INPUT_OBJECT_TYPE_DEFINITION,
+    name: toGraphql_Name(schema.name),
     fields: schema.fields.map((field) => ({
-      kind: graphql.Kind.INPUT_VALUE_DEFINITION,
-      name: { kind: graphql.Kind.NAME, value: field.name },
-      type: nullableIf(field.nullable, toGraphql_NamedType(field.type)),
+      kind: Kind.INPUT_VALUE_DEFINITION,
+      name: toGraphql_Name(field.name),
+      type: toGraphql_Nullable_If(
+        field.nullable,
+        toGraphql_NamedType(field.type)
+      ),
       description: toGraphql_Description(field.description),
     })),
     description: toGraphql_Description(schema.description),
@@ -181,7 +178,7 @@ function toGraphql_from_Schema_Input(
 
 export function toGraphQL(schema: Schema_Index): graphql.DocumentNode {
   return {
-    kind: graphql.Kind.DOCUMENT,
+    kind: Kind.DOCUMENT,
     definitions: Object.values(schema.types).map((type) => {
       switch (type._structype) {
         case "record":
