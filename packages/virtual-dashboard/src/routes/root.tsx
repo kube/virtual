9ab1fed925e75ac92d10501fc6114b366078cae7
Chris Feijoo <chris@kube.io>
2025-04-01 14:19:50 -0400

    Merge SplitView into root

--- packages/virtual-dashboard/src/routes/root.tsx
+++ packages/virtual-dashboard/src/routes/root.tsx
@@ -1,6 +1,7 @@
 import { Logo } from "~/components/Logo";
 import "../app.css";
-import SplitView from "./split";
+import GraphiqlView from "./graphiql";
+import StateView from "./state";
 
 export default function RootView() {
   return (
@@ -13,7 +14,14 @@ export default function RootView() {
       </div>
 
       <div className="flex flex-1">
-        <SplitView />
+        <div className="h-full w-full flex">
+          <div className="flex w-[60%] h-full">
+            <GraphiqlView />
+          </div>
+          <div className="flex w-[40%] h-full">
+            <StateView />
+          </div>
+        </div>
       </div>
     </div>
   );
