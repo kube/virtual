d3cf9e340adcb9216964a909d13009567443f652
Chris Feijoo <chris@kube.io>
2025-04-01 15:15:27 -0400

    Make logo unselectable

--- packages/virtual-dashboard/src/routes/root.tsx
+++ packages/virtual-dashboard/src/routes/root.tsx
@@ -17,7 +17,7 @@ export default function RootView() {
   return (
     <div className="virtualDashboardRoot h-full w-full flex flex-col bg-gray-800">
       <div className="text-white p-1.5 flex items-center justify-between">
-        <div className="flex items-center gap-2 ml-1 opacity-70 h-7">
+        <div className="flex items-center gap-2 ml-1 opacity-70 h-7 select-none">
           <Logo className="w-6 fill-white" />
           <h1>virtual</h1>
         </div>
